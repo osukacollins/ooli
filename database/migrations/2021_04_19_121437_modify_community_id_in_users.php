f80c8017d5b3f1722a68f76e7caf61ff19ebee5c
osukacollins <osukacollins76@gmail.com>
2021-04-19 15:16:49 +0300

    modify users table

---
+++ database/migrations/2021_04_19_121437_modify_community_id_in_users.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ModifyCommunityIdInUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreignId('community_id')->constrained('communities')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreignId('community_id')->constrained('communities');
+        });
+    }
+}
